2996cb43e9ea23c9f2a6898c06e8ad8f63cc1274
adellanno <anthony@dellanno.co.uk>
2015-09-28 18:00:55 +0100

    airport can land a plane

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -5,8 +5,13 @@ describe("Airport", function() {
     airport = new Airport();
   });
 
-  it("should have an aircraft count of 0 upon creation", function(){
+  it("should have an aircraft count of 0 upon creation", function() {
     expect(airport.aircraftCount.length).toEqual(0);
+  });
+
+  it("should be able to land a plane", function() {
+    airport.landPlane("plane");
+    expect(airport.aircraftCount.length).toEqual(1);
   });
 
 
--- src/airport.js
+++ src/airport.js
@@ -1,3 +1,7 @@
 function Airport() {
   this.aircraftCount = [];
 };
+
+Airport.prototype.landPlane = function(plane) {
+  this.aircraftCount.push(plane);
+};
