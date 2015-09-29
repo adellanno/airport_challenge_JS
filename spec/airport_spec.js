d11517e21002f8726a3c2963f780ff66db8cd159
adellanno <anthony@dellanno.co.uk>
2015-09-29 15:01:28 +0100

    airport challenge in javascript completed

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -3,9 +3,8 @@ describe("Airport", function() {
 
   beforeEach(function() {
     airport = new Airport();
-    spyOn(airport, 'weather').and.callThrough();
-    airport.weather.and.stub();
-    weather = ("sunny");
+    plane = new Plane();
+    spyOn(Math, 'random').and.returnValue(0.6);
   });
 
   it("should have an aircraft count of 0 upon creation", function() {
@@ -13,23 +12,38 @@ describe("Airport", function() {
   });
 
   it("should be able to land a plane", function() {
-    airport.landPlane("plane");
+    airport.landPlane(plane);
     expect(airport.aircraftCount.length).toEqual(1);
   });
 
   it("should be able to instruct a plane to take off", function() {
-    airport.landPlane("plane");
-    airport.landPlane("plane");
-    airport.takeOffPlane("plane");
+    airport.landPlane(plane);
+    airport.landPlane(plane);
+    airport.takeOffPlane(plane);
     expect(airport.aircraftCount.length).toEqual(1);
-  })
-
-  it("should not allow a plane to land if the weather is stormy", function() {
-    airport.weather.and.stub();
-    weather = ("sunny");
-    expect(airport.landPlane("plane")).toThrow("stormy!");
   });
 
+  it("should not be able to land any aircraft if at full capacity", function() {
+    for (i = 0; i < airport.capacity; i++) {
+      airport.landPlane(plane);
+    }
+    expect(function(){ airport.landPlane(plane); }).toThrow("Airport at full capacity!");
+  });
+});
 
+describe("Airport", function() {
+  var airport;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = new Plane();
+    spyOn(Math, 'random').and.returnValue(0.4);
+  });
+
+  it("should not allow a plane to land if the weather is stormy", function() {
+    expect(function(){ airport.landPlane(plane); }).toThrow("stormy!");
+  });
 
 });
+
+
--- src/airport.js
+++ src/airport.js
@@ -1,12 +1,17 @@
 function Airport() {
   this.aircraftCount = [];
+  this.capacity = 20;
 };
 
 Airport.prototype.landPlane = function(plane) {
-  if (this.weather == "sunny" ) {
+  if (this.weather() == "stormy") {
+    throw "stormy!";
+  }
+  else if (this.fullCapacity()) {
+    throw "Airport at full capacity!";
+  }
+  else {
     this.aircraftCount.push(plane);
-  } else {
-    throw "sunny!";
   }
 };
 
@@ -15,5 +20,17 @@ Airport.prototype.takeOffPlane = function(plane) {
 };
 
 Airport.prototype.weather = function() {
-  this.weather =  Random(100) > 2 ? "sunny" : "stormy";
+  if (Math.random() > 0.5) {
+    return "sunny";
+  }
+    return "stormy";
 };
+
+Airport.prototype.fullCapacity = function() {
+  if (this.aircraftCount.length >= this.capacity) {
+    return true;
+  }
+    return false;
+};
+
+  // console.log(this);
